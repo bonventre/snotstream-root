0bd6587c4a787ef47db6e002303732035ec0528d
Richie Bonventre <rbonv@hep.upenn.edu>
2012-01-10 15:39:11 -0500

    rate per crate now working too

--- rootapp/mainFrame.C
+++ rootapp/mainFrame.C
@@ -10,6 +10,7 @@
 #include <TApplication.h>
 #include <TGTableLayout.h>
 #include <TGFrame.h>
+#include <TGTab.h>
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -38,7 +39,11 @@ void *mainFrame::thread_draw(void* arg){
   for (Int_t i=0;i<20;i++){
     fCanvas[i]->cd(); 
     f1[i]->Draw("LEGO"); 
+    fCanvasrate[i]->cd();
+    f1rate[i]->Draw("LEGO");
   }
+  fCanvasnhit->cd();
+  f1nhit->Draw();
   TThread::UnLock();
 //  srand ( time(NULL) );
 
@@ -55,8 +60,24 @@ void *mainFrame::thread_draw(void* arg){
     if ((i%upd) == 0) {
       //CURLcode res;
       //res = curl_easy_perform(curl);
-      for (Int_t j=0;j<20;j++)
-        fCanvas[j]->Modified();
+      if (currentTab == 1)
+        for (Int_t j=0;j<20;j++)
+          fCanvas[j]->Modified();
+      if (currentTab == 0)
+        fCanvasnhit->Modified();
+      if (currentTab == 2){
+        for (Int_t j=0;j<20;j++){
+          if (endtime[j] > 0){
+            double elapsed = endtime[j]-starttime[j];
+            for (Int_t k=0;k<16;k++){
+              for (Int_t l=0;l<32;l++){
+                  f1rate[j]->SetBinContent(k+1,l+1,(hits[j*16*32+k*32+l]/elapsed));
+              }
+            }
+          }
+          fCanvasrate[j]->Modified();
+        }
+      }
       usleep(1000000);
     }
   }
@@ -78,6 +99,11 @@ void mainFrame::HandleMenu(Int_t id)
   }
 }
 
+void mainFrame::DoTab(Int_t id)
+{
+  currentTab = id;
+}
+
 void mainFrame::CloseWindow()
 {
   if (AvalancheThread){
@@ -96,12 +122,21 @@ void *mainFrame::thread_avalanche(void* arg)
       if (rec->RecordType == 1){
         RAT::DS::PackedEvent* event = (RAT::DS::PackedEvent*) rec->Rec;
         printf("Got an event with nhit %u\n",event->NHits);
+        f1nhit->Fill(event->NHits);
+        unsigned long clock10part1 = event->MTCInfo[0];
+        unsigned long clock10part2 = event->MTCInfo[1] & 0x1FFFF;
+        double seconds = ((clock10part2 << 32) + clock10part1)/10000000.;
         for (Int_t i=0;i<event->PMTBundles.size();i++){
           RAT::DS::PMTBundle bundle = event->PMTBundles[i];
           int crate = (bundle.Word[0] >> 21) & (((ULong64_t)1 << 5) - 1);
           int card = (bundle.Word[0] >> 26) & (((ULong64_t)1 << 4) - 1);
           int chan = (bundle.Word[0] >> 16) & (((ULong64_t)1 << 5) - 1);
           f1[crate]->Fill(card,chan);
+          hits[crate*16*32+card*32+chan]++;
+          if (starttime[crate] == 0)
+            starttime[crate] = seconds;
+          else
+            endtime[crate] = seconds;
         }
       }
     }
@@ -123,6 +158,7 @@ mainFrame::mainFrame(const TGWindow *p,UInt_t w,UInt_t h) {
   finished = kFALSE;
   thread = 0;
   lastkey = -1;
+  currentTab = 0;
 
   curl = curl_easy_init();
   if(curl) {
@@ -136,7 +172,18 @@ mainFrame::mainFrame(const TGWindow *p,UInt_t w,UInt_t h) {
     char tempname[10];
     sprintf(tempname,"f%02d",i);
     f1[i] = new TH2F(tempname,"This is the Nhit distribution",16,0,16,32,0,32);
+    sprintf(tempname,"f%02d rate",i);
+    f1rate[i] = new TH2F(tempname,"This is the Nhit distribution",16,0,16,32,0,32);
+    starttime[i] = 0;
+    endtime[i] = 0;
   }
+  f1nhit = new TH1F("nhit","Nhit",20,0,400);
+  for (Int_t i=0;i<10000;i++){
+    hits[i] = 0;
+  }
+  
+
+
   AvalancheThread = new TThread("avalanche",(void(*) (void *))&mainFrame::thread_avalanche,(void *)this);
   AvalancheThread->Run();
 
@@ -164,10 +211,17 @@ mainFrame::mainFrame(const TGWindow *p,UInt_t w,UInt_t h) {
   fMenuBar->AddPopup("&Help",fMenuHelp,new TGLayoutHints(kLHintsTop | kLHintsRight) );
   fMain->AddFrame(fMenuBar,new TGLayoutHints(kLHintsTop | kLHintsExpandX));
 
-  TGCompositeFrame *cframe = new TGCompositeFrame(fMain);
-  cframe->SetLayoutManager(new TGTableLayout(cframe,5,4));
+  TGTab *tabframe = new TGTab(fMain,100,100);
+  tabframe->Connect("Selected(Int_t)", "mainFrame",this,"DoTab(Int_t)");
 
-  // Create canvas widget 
+  TGCompositeFrame *cframe2 = tabframe->AddTab("NHit");
+  cframe2->SetLayoutManager(new TGTableLayout(cframe2,2,1));
+  fEcanvasnhit = new TRootEmbeddedCanvas("nhit",cframe2,100,100); 
+  cframe2->AddFrame(fEcanvasnhit,new TGTableLayoutHints(0,1,0,1,kLHintsFillX | kLHintsFillY | kLHintsShrinkX | kLHintsShrinkY | kLHintsExpandX | kLHintsExpandY));
+
+
+  TGCompositeFrame *cframe = tabframe->AddTab("Hits per Channel");
+  cframe->SetLayoutManager(new TGTableLayout(cframe,5,4));
   for (Int_t i=0;i<5;i++){
     for (Int_t j=0;j<4;j++){
       char tempname[10];
@@ -177,7 +231,21 @@ mainFrame::mainFrame(const TGWindow *p,UInt_t w,UInt_t h) {
     }
   }
 
-  fMain->AddFrame(cframe, new TGLayoutHints(kLHintsExpandX| kLHintsExpandY, 
+  TGCompositeFrame *cframe3 = tabframe->AddTab("Hit Rate per Channel");
+  cframe3->SetLayoutManager(new TGTableLayout(cframe3,5,4));
+  for (Int_t i=0;i<5;i++){
+    for (Int_t j=0;j<4;j++){
+      char tempname[10];
+      sprintf(tempname,"%02d rate",i*4+j);
+      fEcanvasrate[i*4+j] = new TRootEmbeddedCanvas(tempname,cframe3,100,100); 
+      cframe3->AddFrame(fEcanvasrate[i*4+j],new TGTableLayoutHints(j,j+1,i,i+1,kLHintsFillX | kLHintsFillY | kLHintsShrinkX | kLHintsShrinkY | kLHintsExpandX | kLHintsExpandY));
+    }
+  }
+
+
+
+  
+  fMain->AddFrame(tabframe, new TGLayoutHints(kLHintsExpandX| kLHintsExpandY, 
         10,10,10,1));  
 
   // Set a name to the main frame 
@@ -189,13 +257,21 @@ mainFrame::mainFrame(const TGWindow *p,UInt_t w,UInt_t h) {
   // Map main frame 
   fMain->MapWindow(); 
 
-  for (Int_t i=0;i<20;i++)
+  for (Int_t i=0;i<20;i++){
     fCanvas[i] = fEcanvas[i]->GetCanvas();  
+    fCanvasrate[i] = fEcanvasrate[i]->GetCanvas();  
+  }
+  fCanvasnhit = fEcanvasnhit->GetCanvas();
 
   while (!finished) {
-    for (Int_t i=0;i<20;i++)
+    for (Int_t i=0;i<20;i++){
       if (fCanvas[i]->IsModified())
         fCanvas[i]->Update();
+      if (fCanvasrate[i]->IsModified())
+        fCanvasrate[i]->Update();
+    }
+    if (fCanvasnhit->IsModified())
+      fCanvasnhit->Update();
 
     gSystem->ProcessEvents();
     usleep(100);
--- rootapp/mainFrame.h
+++ rootapp/mainFrame.h
@@ -19,13 +19,23 @@ class mainFrame {
   private: 
     TGMainFrame         *fMain; 
     TRootEmbeddedCanvas *fEcanvas[20]; 
+    TRootEmbeddedCanvas *fEcanvasrate[20]; 
+    TRootEmbeddedCanvas *fEcanvasnhit; 
     TCanvas *fCanvas[20];  
+    TCanvas *fCanvasrate[20];  
+    TCanvas *fCanvasnhit;  
+    TH2F *f1[20];
+    TH2F *f1rate[20];
+    TH1F *f1nhit;
+    double starttime[20];
+    double endtime[20];
+    Int_t hits[10000];
     TThread             *thread;
     TThread             *AvalancheThread;
     Bool_t finished;
     CURL *curl;
     int lastkey;
-    TH2F *f1[20];
+    int currentTab;
 
     enum menu_identifiers {
       M_FILE_OPEN,
@@ -41,6 +51,7 @@ class mainFrame {
     void *thread_avalanche(void* arg);
     void CloseWindow();
     void HandleMenu(Int_t id);
+    void DoTab(Int_t id);
     void curl_write(char *ptr);
 
 };
